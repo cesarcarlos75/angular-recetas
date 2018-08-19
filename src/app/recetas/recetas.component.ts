b90714bcd42e9f9ff524712049e119bbda43cc2f
Cesar Puch <cesarcarlos@Cesars-MacBook-Pro.local>
2018-08-19 16:48:36 -0500

    First exercise: add to array and show full list

--- src/app/recetas/recetas.component.ts
+++ src/app/recetas/recetas.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecetasComponent implements OnInit {
 
+  recetas: {}[] = [];
+  recetaTitulo: string;
+  recetaDesc: string;
+  recetaURL: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  capturarTitulo(evt){
+    this.recetaTitulo = evt;
+  }
+  capturarDesc(evt){
+    this.recetaDesc = evt;
+  }
+  capturarURL(evt){
+    this.recetaURL = evt;
+  }
+
+  agregarReceta(){
+    
+    if (this.recetaTitulo && this.recetaTitulo.trim() != "" && this.recetaDesc && this.recetaDesc.trim() != "") {
+      const receta = {titulo: this.recetaTitulo, descripcion: this.recetaDesc, url: this.recetaURL};
+      this.recetas.push(receta);
+    }
+
+    //Limpiar
+    this.recetaTitulo = "";
+    this.recetaDesc = "";
+    this.recetaURL = "";
+
   }
 
 }
